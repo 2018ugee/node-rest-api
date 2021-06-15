b37d4d01c1c8704c35ce8f09cbb1649a2425bbb8
Pritam Kumar <66910624+2018ugee@users.noreply.github.com>
2021-06-16 04:28:40 +0530

    made change in index.js client word

--- index.js
+++ index.js
@@ -70,9 +70,11 @@ app.get("/pritam", (req, res) => {
 
 //heroku code for deploy
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("react-chat-app/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+    res.sendFile(
+      path.resolve(__dirname, "react-chat-app", "build", "index.html")
+    );
   });
 }
 
